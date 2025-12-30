418180d6c7f6411b550aef3d5d54b120e5480df7
Yim Toldara <ytoldara@mef.gov.kh>
2025-12-30 21:49:08 +0700

    update fle directory

--- starter-project/infrastructure/terraform-manifest/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mef-final-devops-bucket"
-    key            = "terraform/terraform.state"
-    region         = "us-east-1"
-    dynamodb_table = "mef-final-terraform-state"
-  }
-}
-
